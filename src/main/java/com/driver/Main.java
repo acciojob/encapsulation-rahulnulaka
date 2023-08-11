2bb3df008ae411ecc5103811113613f8b425963a
rahul <rahulsunny988@gmail.com>
2023-08-11 16:28:53 +0530

    commit changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
+import com.driver.RWOnly;
+
 public class Main {
-  
+	public static void main(String[] args) {
+		RWOnly r = new RWOnly();
+		// System.out.println(r.name("Rahul"));
+		// Error: Could not find or load main class
+		r.setName("Rahul");
+		System.out.println(r.getName());
+	}
 }
